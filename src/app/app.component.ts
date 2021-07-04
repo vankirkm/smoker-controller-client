5e0aa070e04693b45c0f679b5b044c670fd699f1
Michael Van Kirk <vankirk.6@wright.edu>
2021-07-04 17:43:12 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, throwError, interval } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { TemplateBindingParseResult } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'smoker-controller';
+  currentTemp: any;
+  currentSetTemp: any;
+  tempUrl = 'http://localhost:3000/api/current-temp';
+  ;
+
+  constructor(private http: HttpClient,){
+  }
+
+  
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+
+    interval(1000).subscribe(() => {
+      this.getCurrentTemp();
+    });
+    
+  }
+
+  getCurrentTemp(): void{
+    this.http.get(this.tempUrl).subscribe(val => this.currentTemp = val);
+  }
+
+  changeSetTemp(setTemp: any): void{
+    console.log('changing set temp from client to ' + setTemp);
+    this.currentSetTemp = setTemp;
+    this.http.post<any>('http://localhost:3000/api/change-set-temp', this.currentSetTemp)
+      .subscribe((result) => console.log(result));;
+  }
+
+  getSetTemp(): void{
+    this.http.get('http://localhost:3000/api/get-set-temp').subscribe(val => {
+      this.currentSetTemp = val;
+      console.log('received set temp data: ' + this.currentSetTemp);
+    });
+  }
+  
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button'
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatCardModule,
     MatListModule,
     MatTabsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    HttpClientModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
